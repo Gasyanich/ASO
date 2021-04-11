0f1fb9b6666c56d3303b225ee095ce8a8b523bf5
Artyom <artyom.paskin@gmail.com>
2021-04-12 02:05:55 +0400

    Add sidebar icons

--- ASO.API/ClientApp/src/app/common-layout/sidebar/sidebar-item.ts
+++ ASO.API/ClientApp/src/app/common-layout/sidebar/sidebar-item.ts
@@ -1,11 +1,15 @@
+import { IconDefinition } from "@fortawesome/fontawesome-common-types";
+
 export class SidebarItem {
   public name: string;
   public path?: string;
+  public icon?: IconDefinition;
   private onClick?: () => {};
 
-  constructor(name: string, path?: string, callback?: () => {}) {
+  constructor(name: string, path?: string, icon?: IconDefinition, callback?: () => {}) {
     this.name = name;
     if (path) this.path = path;
     if (callback) this.onClick = callback;
+    if (icon) this.icon = icon;
   }
 }
