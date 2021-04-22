c37d26982d304b4f78666cb296213f3df83d9724
Gasyanich <svv-66@yandex.ru>
2021-04-22 19:58:06 +0400

    Request models validation

---
+++ ASO.Models/Constants/ErrorMessageConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ASO.Models.Constants
+{
+    public class ErrorMessageConstants
+    {
+        public const string RequiredField = "Поле должно быть заполнено";
+        public const string IncorrectEmail = "Некорректный email";
+
+    }
+}
